8983b7ce87e1dc6ec83a3d1a7d113564765a5217
Kostya Oreshin <koreshin67@gmail.com>
2025-10-28 12:42:34 +0300

    test: add tests for managers

--- src/engine/scene/object_manager.rs
+++ src/engine/scene/object_manager.rs
@@ -39,153 +39,160 @@ impl GameObjectFactory {
     }
 }
 
-// mod game_object_factory_tests {
-//     use super::*;
-//     struct MockComponent;
-//     impl Component for MockComponent {
+#[cfg(test)]
+mod factory_tests {
+    use super::*;
+    use crate::engine::scene::game_object::components::sprite::Sprite;
+    
+    fn create_test_position(x: i32, y: i32, z: i32, is_relative: bool) -> Position {
+        Position { x, y, z, is_relative }
+    }
+    
+    fn create_test_components() -> Vec<Box<dyn Component>> {
+        vec![Box::new(Sprite::new(None))]
+    }
+    
+    #[test]
+    fn test_new_creates_factory() {
+        let factory = GameObjectFactory::new(100);
         
-//         fn as_any(&self) -> &dyn std::any::Any {
-//             todo!()
-//         }
+        assert_eq!(factory.max_objects, 100);
+        assert_eq!(factory.allocated_objects, 0);
+        assert!(factory.uids.is_empty());
+    }
+    
+    #[test]
+    fn test_new_with_zero_max_objects() {
+        let factory = GameObjectFactory::new(0);
         
-//         fn get_component_type(&self) -> crate::engine::scene::game_object::components::ComponentType {
-//             todo!()
-//         }
+        assert_eq!(factory.max_objects, 0);
+        assert_eq!(factory.allocated_objects, 0);
+        assert!(factory.uids.is_empty());
+    }
+    
+    #[test]
+    fn test_create_object_returns_unique_uid() {
+        let mut factory = GameObjectFactory::new(10);
+        let components = create_test_components();
+        let position = create_test_position(0, 0, 0, false);
         
-//         fn get_sprite_unchecked(&self) -> &Option<image::DynamicImage> {
-//             &None
-//         }
-//     }
+        let (uid, _obj) = factory.create_object(components, position);
+        
+        assert_eq!(uid, 1);
+        assert_eq!(factory.allocated_objects, 1);
+    }
+    
+    
+    #[test]
+    fn test_create_object_returns_game_object_with_correct_position() {
+        let mut factory = GameObjectFactory::new(10);
+        let position = create_test_position(10, 20, 30, false);
+        
+        let (_uid, obj) = factory.create_object(create_test_components(), position);
+        
+        assert_eq!(obj.position.x, 10);
+        assert_eq!(obj.position.y, 20);
+        assert_eq!(obj.position.z, 30);
+    }
+    
+    #[test]
+    fn test_create_object_returns_game_object_with_components() {
+        let mut factory = GameObjectFactory::new(10);
+        let components = vec![
+            Box::new(Sprite::new(None)) as Box<dyn Component>,
+            Box::new(Sprite::new(None)) as Box<dyn Component>,
+        ];
+        
+        let (_uid, obj) = factory.create_object(components, create_test_position(0, 0, 0, false));
+        
+        assert_eq!(obj.components.len(), 2);
+    }
+    
+    #[test]
+    #[should_panic(expected = "Trying to create object above limit")]
+    fn test_create_object_panics_when_exceeding_limit() {
+        let mut factory = GameObjectFactory::new(2);
+        
+        factory.create_object(create_test_components(), create_test_position(0, 0, 0, false));
+        factory.create_object(create_test_components(), create_test_position(1, 1, 1, false));
+        
+        factory.create_object(create_test_components(), create_test_position(2, 2, 2, false));
+    }
 
-//     #[test]
-//     fn test_new_factory_init() {
-//         let factory = GameObjectFactory::new(7);
-//         assert_eq!(factory.max_objects, 7);
-//         assert_eq!(factory.allocated_objects, 0);
-//         assert!(factory.uids.is_empty())
-//     }
+    #[test]
+    fn test_create_object_reuses_freed_uid() {
+        let mut factory = GameObjectFactory::new(10);
+        
+        factory.create_object(create_test_components(), create_test_position(0, 0, 0, false));
 
-//     #[test]
-//     fn test_create_object() {
-//         let mut factory = GameObjectFactory::new(5);
-//         let components: Vec<Box<dyn Component>> = vec![Box::new(MockComponent)];
-//         let position = Position { x: 10, y: 20, z: 15, is_relative: false };
-        
-//         let (uid, obj) = factory.create_object(components, position);
-        
-//         assert_eq!(uid, 1);
-//         assert_eq!(factory.allocated_objects, 1);
-//         assert!(factory.uids.is_empty());
-//     }
+        factory.uids.insert(1);
 
-//     #[test]
-//     fn test_create_multiple_objects_within_limit() {
-//         let mut factory = GameObjectFactory::new(3);
+        let (uid, _) = factory.create_object(create_test_components(), create_test_position(1, 1, 1, false));
         
-//         let (uid1, _) = factory.create_object(
-//             vec![Box::new(MockComponent)],
-//             Position { x: 0.0, y: 0.0 }
-//         );
-//         let (uid2, _) = factory.create_object(
-//             vec![Box::new(MockComponent)],
-//             Position { x: 1.0, y: 1.0 }
-//         );
-//         let (uid3, _) = factory.create_object(
-//             vec![Box::new(MockComponent)],
-//             Position { x: 2.0, y: 2.0 }
-//         );
+        assert_eq!(uid, 1);
+        assert!(factory.uids.is_empty());
+        assert_eq!(factory.allocated_objects, 1);
+    }
+    
+    #[test]
+    fn test_create_object_reuses_multiple_freed_uids() {
+        let mut factory = GameObjectFactory::new(10);
         
-//         assert_eq!(uid1, 1);
-//         assert_eq!(uid2, 2);
-//         assert_eq!(uid3, 3);
-//         assert_eq!(factory.allocated_objects, 3);
-//     }
-
-//     #[test]
-//     #[should_panic(expected = "Trying to create object above limit")]
-//     fn test_create_object_exceeds_limit() {
-//         let mut factory = GameObjectFactory::new(2);
+        factory.create_object(create_test_components(), create_test_position(0, 0, 0, false));
+        factory.create_object(create_test_components(), create_test_position(1, 1, 1, false));
+        factory.create_object(create_test_components(), create_test_position(2, 2, 2, false));
         
-//         // Создаем два объекта - достигаем лимита
-//         factory.create_object(
-//             vec![Box::new(MockComponent)],
-//             Position { x: 0.0, y: 0.0 }
-//         );
-//         factory.create_object(
-//             vec![Box::new(MockComponent)],
-//             Position { x: 1.0, y: 1.0 }
-//         );
+        factory.uids.insert(1);
+        factory.uids.insert(2);
         
-//         // Третий объект должен вызвать панику
-//         factory.create_object(
-//             vec![Box::new(MockComponent)],
-//             Position { x: 2.0, y: 2.0 }
-//         );
-//     }
-
-//     #[test]
-//     fn test_reuse_uid_from_pool() {
-//         let mut factory = GameObjectFactory::new(5);
+        let (uid1, _) = factory.create_object(create_test_components(), create_test_position(3, 3, 3, false));
+        let (uid2, _) = factory.create_object(create_test_components(), create_test_position(4, 4, 4, false));
         
-//         // Создаем объект
-//         let (uid1, _) = factory.create_object(
-//             vec![Box::new(MockComponent)],
-//             Position { x: 0.0, y: 0.0 }
-//         );
+        assert!(uid1 == 1 || uid1 == 2);
+        assert!(uid2 == 1 || uid2 == 2);
+        assert_ne!(uid1, uid2);
+        assert!(factory.uids.is_empty());
+    }
+    
+    #[test]
+    fn test_create_object_with_freed_uids_at_limit() {
+        let mut factory = GameObjectFactory::new(2);
         
-//         // Вручную добавляем UID обратно в пул (имитация удаления объекта)
-//         factory.uids.insert(uid1);
+        factory.create_object(create_test_components(), create_test_position(0, 0, 0, false));
+        factory.create_object(create_test_components(), create_test_position(1, 1, 1, false));
         
-//         // Создаем новый объект - должен использовать освобожденный UID
-//         let (uid2, _) = factory.create_object(
-//             vec![Box::new(MockComponent)],
-//             Position { x: 1.0, y: 1.0 }
-//         );
+        factory.uids.insert(1);
         
-//         assert_eq!(uid1, uid2);
-//         assert!(factory.uids.is_empty());
-//         // allocated_objects не должен увеличиться
-//         assert_eq!(factory.allocated_objects, 1);
-//     }
-
-//     #[test]
-//     fn test_reuse_multiple_uids() {
-//         let mut factory = GameObjectFactory::new(10);
+        let (uid, _) = factory.create_object(create_test_components(), create_test_position(2, 2, 2, false));
         
-//         // Создаем несколько объектов
-//         let (uid1, _) = factory.create_object(
-//             vec![Box::new(MockComponent)],
-//             Position { x: 0.0, y: 0.0 }
-//         );
-//         let (uid2, _) = factory.create_object(
-//             vec![Box::new(MockComponent)],
-//             Position { x: 1.0, y: 1.0 }
-//         );
+        assert_eq!(uid, 1);
+        assert_eq!(factory.allocated_objects, 2);
+    }
+    
+    #[test]
+    fn test_create_object_with_empty_components() {
+        let mut factory = GameObjectFactory::new(10);
         
-//         // Освобождаем оба UID
-//         factory.uids.insert(uid1);
-//         factory.uids.insert(uid2);
+        let (_uid, obj) = factory.create_object(vec![], create_test_position(0, 0, 0, false));
         
-//         // Создаем два новых объекта
-//         let (reused_uid1, _) = factory.create_object(
-//             vec![Box::new(MockComponent)],
-//             Position { x: 2.0, y: 2.0 }
-//         );
-//         let (reused_uid2, _) = factory.create_object(
-//             vec![Box::new(MockComponent)],
-//             Position { x: 3.0, y: 3.0 }
-//         );
+        assert_eq!(obj.components.len(), 0);
+    }
+    
+    #[test]
+    fn test_uid_reuse_priority() {
+        let mut factory = GameObjectFactory::new(10);
         
-//         // Проверяем, что использованы старые UID
-//         assert!(reused_uid1 == uid1 || reused_uid1 == uid2);
-//         assert!(reused_uid2 == uid1 || reused_uid2 == uid2);
-//         assert_ne!(reused_uid1, reused_uid2);
-//         assert!(factory.uids.is_empty());
-//     }
-
-
-// }
-
+        factory.create_object(create_test_components(), create_test_position(0, 0, 0, false));
+        factory.create_object(create_test_components(), create_test_position(1, 1, 1, false));
+        
+        factory.uids.insert(5);
+        
+        let (uid, _) = factory.create_object(create_test_components(), create_test_position(2, 2, 2, false));
+        
+        assert_eq!(uid, 5);
+        assert!(factory.uids.is_empty());
+    }
+}
 pub struct GameObjectManager {
     pub game_objects: HashMap<usize, GameObject>,
     factory: GameObjectFactory,
@@ -203,4 +210,125 @@ impl GameObjectManager {
         let (uid, object) = self.factory.create_object(components, position);
         self.game_objects.insert(uid, object);
     }
+}
+
+#[cfg(test)]
+mod manager_tests {
+    use crate::engine::scene::game_object::components::sprite::Sprite;
+
+    use super::*;
+    
+    fn create_test_position(x: i32, y: i32, z: i32, is_relative: bool) -> Position {
+        Position { x, y, z, is_relative }
+    }
+    
+    fn create_test_components() -> Vec<Box<dyn Component>> {
+        vec![Box::new(Sprite::new(None))]
+    }
+    
+    
+    #[test]
+    fn test_new_with_different_capacities() {
+        let manager1 = GameObjectManager::new(0);
+        let manager2 = GameObjectManager::new(100);
+        let manager3 = GameObjectManager::new(1000);
+        
+        assert_eq!(manager1.game_objects.len(), 0);
+        assert_eq!(manager2.game_objects.len(), 0);
+        assert_eq!(manager3.game_objects.len(), 0);
+    }
+
+    
+    #[test]
+    fn test_add_game_object_adds_to_hashmap() {
+        let mut manager = GameObjectManager::new(10);
+        
+        manager.add_game_object(
+            create_test_components(),
+            create_test_position(0, 0, 0, false),
+        );
+        
+        assert_eq!(manager.game_objects.len(), 1);
+        assert!(manager.game_objects.contains_key(&1));
+    }
+    
+    #[test]
+    fn test_add_game_object_stores_correct_position() {
+        let mut manager = GameObjectManager::new(10);
+        let position = create_test_position(15, 25, 35, false);
+        
+        manager.add_game_object(create_test_components(), position);
+        
+        let obj = manager.game_objects.get(&1).unwrap();
+        assert_eq!(obj.position.x, 15);
+        assert_eq!(obj.position.y, 25);
+        assert_eq!(obj.position.z, 35);
+    }
+    
+    #[test]
+    fn test_add_game_object_with_empty_components() {
+        let mut manager = GameObjectManager::new(10);
+        
+        manager.add_game_object(vec![], create_test_position(0, 0, 0, false));
+        
+        assert_eq!(manager.game_objects.len(), 1);
+        let obj = manager.game_objects.get(&1).unwrap();
+        assert_eq!(obj.components.len(), 0);
+    }
+    
+    #[test]
+    #[should_panic(expected = "Trying to create object above limit")]
+    fn test_add_game_object_with_zero_limit() {
+        let mut manager = GameObjectManager::new(0);
+
+        manager.add_game_object(
+            create_test_components(),
+            create_test_position(0, 0, 0, false),
+        );
+    }
+
+    
+    #[test]
+    fn test_add_game_object_with_negative_positions() {
+        let mut manager = GameObjectManager::new(10);
+        
+        manager.add_game_object(
+            create_test_components(),
+            create_test_position(-10, -20, -30, false),
+        );
+        
+        let obj = manager.game_objects.get(&1).unwrap();
+        assert_eq!(obj.position.x, -10);
+        assert_eq!(obj.position.y, -20);
+        assert_eq!(obj.position.z, -30);
+    }
+
+
+    
+    #[test]
+    fn test_manager_can_retrieve_objects_by_uid() {
+        let mut manager = GameObjectManager::new(10);
+        
+        manager.add_game_object(
+            create_test_components(),
+            create_test_position(100, 200, 300, false),
+        );
+        
+        let retrieved = manager.game_objects.get(&1);
+        assert!(retrieved.is_some());
+        
+        let obj = retrieved.unwrap();
+        assert_eq!(obj.position.x, 100);
+        assert_eq!(obj.position.y, 200);
+        assert_eq!(obj.position.z, 300);
+    }
+    
+    #[test]
+    fn test_manager_returns_none_for_nonexistent_uid() {
+        let manager = GameObjectManager::new(10);
+        
+        let retrieved = manager.game_objects.get(&999);
+        assert!(retrieved.is_none());
+    }
+
 }
--- src/engine/scene_manager.rs
+++ src/engine/scene_manager.rs
@@ -21,3 +21,156 @@ impl SceneManager {
         self.active_scene.init()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::engine::scene::game_object::{Object, Position, components::{Component, sprite::Sprite}};
+
+    use super::*;
+    
+    fn create_test_position(x: i32, y: i32, z: i32, is_relative: bool) -> Position {
+        Position { x, y, z, is_relative }
+    }
+    
+    fn create_test_components() -> Vec<Box<dyn Component>> {
+        vec![Box::new(Sprite::new(None))]
+    }
+    
+    fn create_simple_scene() -> Scene {
+        Scene::new(
+            vec![],
+            create_test_components(),
+            create_test_position(0, 0, 0, false),
+        )
+    }
+
+    fn create_scene_with_sprites(sprite_count: usize) -> Scene {
+        let mut objects = vec![];
+        
+        for i in 0..sprite_count {
+            let obj = GameObject::new(
+                create_test_components(),
+                create_test_position(i as i32, i as i32, i as i32, false),
+            );
+            objects.push(obj);
+        }
+        
+        Scene::new(objects, vec![], create_test_position(0, 0, 0, false))
+    }
+    
+    #[test]
+    fn test_new_stores_provided_scene() {
+        let scene = Scene::new(
+            vec![],
+            create_test_components(),
+            create_test_position(10, 20, 30, false),
+        );
+        
+        let manager = SceneManager::new(scene);
+        
+        assert_eq!(manager.active_scene.main_object.position.x, 10);
+        assert_eq!(manager.active_scene.main_object.position.y, 20);
+        assert_eq!(manager.active_scene.main_object.position.z, 30);
+    }
+    
+    #[test]
+    fn test_active_scene_returns_same_scene() {
+        let scene = Scene::new(
+            vec![],
+            create_test_components(),
+            create_test_position(15, 25, 35, false),
+        );
+        
+        let manager = SceneManager::new(scene);
+        let active = manager.active_scene();
+        
+        assert_eq!(active.main_object.position.x, 15);
+        assert_eq!(active.main_object.position.y, 25);
+        assert_eq!(active.main_object.position.z, 35);
+    }
+    
+    #[test]
+    fn test_init_active_scene_returns_empty_for_scene_without_sprites() {
+        let scene = create_simple_scene();
+        let manager = SceneManager::new(scene);
+        
+        let renderable = manager.init_active_scene();
+        
+        assert_eq!(renderable.len(), 0);
+    }
+    
+    // #[test]
+    // fn test_init_active_scene_returns_sprites() {
+    //     let scene = create_scene_with_sprites(1);
+    //     let manager = SceneManager::new(scene);
+        
+    //     let renderable = manager.init_active_scene();
+        
+    //     assert_eq!(renderable.len(), 1);
+    // }
+    
+    #[test]
+    fn test_scene_manager_with_empty_scene() {
+        let scene = Scene::new(vec![], vec![], create_test_position(0, 0, 0, false));
+        let manager = SceneManager::new(scene);
+        
+        let renderable = manager.init_active_scene();
+        
+        assert_eq!(renderable.len(), 0);
+    }
+    
+    // #[test]
+    // fn test_scene_manager_with_scene_containing_objects_without_sprites() {
+    //     let obj1 = GameObject::new(
+    //         create_test_components(),
+    //         create_test_position(0, 0, 0, false),
+    //     );
+    //     let obj2 = GameObject::new(
+    //         create_test_components(),
+    //         create_test_position(10, 10, 10, false),
+    //     );
+        
+    //     let scene = Scene::new(
+    //         vec![obj1, obj2],
+    //         vec![],
+    //         create_test_position(0, 0, 0, false),
+    //     );
+    //     let manager = SceneManager::new(scene);
+        
+    //     let renderable = manager.init_active_scene();
+        
+    //     assert_eq!(renderable.len(), 0);
+    // }
+    
+    #[test]
+    fn test_active_scene_preserves_scene_structure() {
+        let obj1 = GameObject::new(
+            create_test_components(),
+            create_test_position(1, 2, 3, false),
+        );
+        let obj2 = GameObject::new(
+            create_test_components(),
+            create_test_position(4, 5, 6, false),
+        );
+        
+        let scene = Scene::new(
+            vec![obj1, obj2],
+            vec![],
+            create_test_position(7, 8, 9, false),
+        );
+        
+        let manager = SceneManager::new(scene);
+        let active = manager.active_scene();
+        assert_eq!(active.main_object.position.x, 7);
+    }
+    
+    
+    #[test]
+    fn test_active_scene_is_immutable_reference() {
+        let scene = create_simple_scene();
+        let manager = SceneManager::new(scene);
+        
+        let _active = manager.active_scene();
+        let _active2 = manager.active_scene();
+    }
+}
